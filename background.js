87c16a0d11c7104c63d955175135ba61d92c8fdb
thoratica <me@tica.fun>
2023-01-18 19:51:10 +0900

    설치 시 기본 이모티콘 추가 및 스토어 링크 열기

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
+chrome.runtime.onInstalled.addListener(async (details) => {
+  if (details.reason == "install") {
+    const ids = (await chrome.storage.sync.get(["emoticons"])).emoticons ?? [];
+    if (ids.length === 0) {
+      await chrome.storage.sync.set({ emoticons: ["6049a7d7cea5c400506e9bee", "63a15d244a098f0076fbcf6f"] });
+      chrome.tabs.create({ url: "https://dutmoticon.tica.fun" });
+    }
+  }
+});
+
 chrome.tabs.onUpdated.addListener((tabId, info) => {
   if (
     !info.url?.startsWith("http://localhost:3000/emoticon/") &&
